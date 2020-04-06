580a0423e56fe0d91771d1c4811f80b67becb0ee
dianakw8591 <dianakw@gmail.com>
2020-04-06 11:51:47 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+        {actors.map(actor => {
+          return (
+          <div key={actor.name}>
+            <h4>{actor.name}</h4>
+            <ul>{actor.movies.map(movie => {
+              return <li>{movie}</li>
+            })}</ul>
+          </div>)
+        })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+          <div key={movie.name}>
+            <h4>{movie.title}</h4>
+            <h5>{movie.time} minutes</h5>
+            <ul>{movie.genres.map(genre => {
+              return <li>{genre}</li>
+            })}</ul>
+          </div>)
+        })}
     </div>
   );
 };
